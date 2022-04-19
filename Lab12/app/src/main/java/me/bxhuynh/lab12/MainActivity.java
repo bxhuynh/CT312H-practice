ede360b82ff8a8fa67fae982e53a45105ba3fe46
Bui Xuan Huynh <buixuanhuynha1@gmail.com>
2022-04-19 14:11:09 +0700

    lab12: gfg saving data

---
+++ Lab12/app/src/main/java/me/bxhuynh/lab12/MainActivity.java
@@ -0,0 +1,85 @@
+package me.bxhuynh.lab12;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText employeeNameEdt, employeePhoneEdt, employeeAddressEdt;
+    private Button sendDatabtn;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    EmployeeInfo employeeInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        employeeNameEdt = findViewById(R.id.idEdtEmployeeName);
+        employeePhoneEdt = findViewById(R.id.idEdtEmployeePhoneNumber);
+        employeeAddressEdt = findViewById(R.id.idEdtEmployeeAddress);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("EmployeeInfo");
+        employeeInfo = new EmployeeInfo();
+        sendDatabtn = findViewById(R.id.idBtnSendData);
+
+        sendDatabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // getting text from our edittext fields.
+                String name = employeeNameEdt.getText().toString();
+                String phone = employeePhoneEdt.getText().toString();
+                String address = employeeAddressEdt.getText().toString();
+
+
+                // below line is for checking weather the
+                // edittext fields are empty or not.
+                if (TextUtils.isEmpty(name) && TextUtils.isEmpty(phone) && TextUtils.isEmpty(address)) {
+                    // if the text fields are empty
+                    // then show the below message.
+                    Toast.makeText(MainActivity.this, "Please add some data.", Toast.LENGTH_SHORT).show();
+                } else {
+                    // else call the method to add
+                    // data to our database.
+                    addDatatoFirebase(name, phone, address);
+                }
+            }
+        });
+    }
+
+    private void addDatatoFirebase(String name, String phone, String address) {
+        employeeInfo.setEmployeeName(name);
+        employeeInfo.setEmployeeContactNumber(phone);
+        employeeInfo.setEmployeeAddress(address);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                databaseReference.setValue(employeeInfo);
+
+                Toast.makeText(MainActivity.this, "data added", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, "Fail to add data " + error, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ Lab12/app/src/main/java/me/bxhuynh/lab12/User.java
@@ -0,0 +1,19 @@
+package me.bxhuynh.lab12;
+
+public class User {
+    public  String name;
+    public String address;
+
+    public User()
+    {
+        name = "";
+        address = "";
+    }
+
+    public User(String name, String address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+}
